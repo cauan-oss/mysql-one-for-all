9f4c980af32b8ac64eba6353fcec7c7371c67a81
cauan <cauanbarros14@gmail.com>
2023-03-11 19:52:30 -0300

    corrigindo o req4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -3,7 +3,7 @@ SELECT
     IF(MAX(YEAR(historico_data_reproducao)) >= 2021,
 		'Ativa',
         'Inativa') AS status_pessoa_usuaria
-FROM SpotifyClone.pessoa AS pe
+FROM SpotifyClone.usuario AS pe
 JOIN SpotifyClone.historico AS hi ON hi.pessoa_id = pe.pessoa_id
 GROUP BY pe.pessoa_nome
 ORDER BY pe.pessoa_nome;
