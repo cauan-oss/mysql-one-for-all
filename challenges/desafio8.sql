ba7c817f56a6c9d5a76ec1c6bcf26d302fb0b0d4
cauan <cauanbarros14@gmail.com>
2023-03-11 20:15:35 -0300

    algumas alteracoes

--- challenges/desafio8.sql
+++ challenges/desafio8.sql
@@ -5,5 +5,5 @@ FROM
 	SpotifyClone.artistas AS ar
 JOIN
 	SpotifyClone.album AS al ON al.artista_id = ar.artista_id
-WHERE ar.artista_nome = 'ELis Regina'
+WHERE ar.artista_nome = 'Baco Exu do Blues'
 ORDER BY al.album_nome;
