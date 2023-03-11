819d177aec48bf4cd25f7600a87acdd009f91502
cauan <cauanbarros14@gmail.com>
2023-03-11 19:28:05 -0300

    reqs

--- challenges/desafio.3.sql
+++ challenges/desafio.3.sql
@@ -1,8 +1,8 @@
 SELECT 
-	pe.pessoa_nome AS pessoa_usuaria,
+	pe.usuario AS pessoa_usuaria,
     COUNT(hi.cancao_id) AS musicas_ouvidas,
     ROUND(SUM(ca.cancao_duracao_segundos / 60), 2) AS total_minutos
-FROM SpotifyClone.pessoa pe
+FROM SpotifyClone.usuario pe
 JOIN SpotifyClone.historico hi ON hi.pessoa_id = pe.pessoa_id
 JOIN SpotifyClone.cancoes ca ON ca.cancao_id = hi.cancao_id
 GROUP BY pe.pessoa_nome
